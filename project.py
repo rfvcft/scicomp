32af119c4b7062834618071bf9c4d90682ed0e58
Nils Thorin <thorinns@iadm133.mathematik.uni-stuttgart.de>
2025-06-03 21:02:09 +0200

    fixed the boundary conditions a bit, and did some very basic animations

--- project.py
+++ project.py
@@ -4,6 +4,8 @@ from typing import Callable
 import numpy as np
 from numpy import linalg as LA
 from scipy.sparse import lil_matrix
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
 class Grid:
     """
@@ -12,9 +14,18 @@ class Grid:
     def __init__(self, N : int):
         self.N = N
         self.dx = 1 / (N+1)
+        self.boundary_points = defaultdict(list)
     
     def n(self, i : int, j : int) -> int:
         return self.N*i + j
+
+    def generate_inner_points(self) -> np.ndarray:
+        inner_points = np.zeros((self.N**2, 2))
+        for i in range(N):
+            for j in range(N):
+                # Translate the inner point into array form, but keep the x- and y- values
+                inner_points[self.n(i, j)] = [(i+1)*self.dx, (j+1)*self.dx]
+        return inner_points
 
     # "Intuitive" creation of Laplacian
     def slow_laplacian(self) -> np.matrix:
@@ -30,7 +41,7 @@ class Grid:
                     L[self.n(i,j)][self.n(i,j-1)] = 1
                 if j != self.N-1:
                     L[self.n(i,j)][self.n(i,j+1)] = 1
-        return L
+        return (1/self.dx)**2 * L
     
     # Laplacian implemented as sparse matrix
     # Computes self.boundary_points which maps indices n to corresponding boundary points (i, j)
@@ -49,9 +60,9 @@ class Grid:
                     if in_bounds(i_, j_):
                         L[self.n(i, j), self.n(i_, j_)] = 1
                     else:
-                        boundary_points[self.n(i, j)].append((i_, j_))
+                        boundary_points[self.n(i, j)].append(((i_+1)*self.N, (j_+1)*self.N))
         self.boundary_points = boundary_points
-        return L
+        return (1/self.dx)**2 * L
 
     
     # Creation of Laplacian using diagonals
@@ -65,7 +76,7 @@ class Grid:
             L[i*self.N][i*self.N - 1] = 0
             L[i*self.N - 1][i*self.N] = 0
 
-        return L
+        return (1/self.dx)**2 * L
     
     def test_speeds(self):
         # Somehow, the "slow" laplacian is a lot faster than the "fast" laplacian?? I don't get it, but sure...
@@ -83,7 +94,7 @@ class Grid:
 
 class TimeStepper:
     
-    def __init__(self, N : int, M : int, final_time : int, u_init : np.ndarray, f : Callable[[np.ndarray, float], np.ndarray], g : Callable):
+    def __init__(self, N : int, M : int, final_time : int, u_init : np.ndarray, f : Callable[[np.ndarray, float], np.ndarray], g : Callable[[dict, float], np.ndarray]):
         """
         N - Grid Size
         M - Number of time steps
@@ -101,23 +112,9 @@ class TimeStepper:
 
         self.dx = 1 / (N+1)
         self.dt = 1 / (M-1)
-        self.g = Grid(self.N)
-        self.A = np.eye(N**2) - self.dt * self.g.laplacian()
-        self.inner_points = self.generate_inner_points()
-        self.boundary_points = self.generate_boundary_points()
-    
-    def generate_boundary_points(self):
-        # TODO: Create this method which returns the points that are on the boundary, in an array which corresponds to where the boundary values need to land 
-        # (does this make any sense?, should it be implemented in some other way perhaps?)
-        pass
-
-    def generate_inner_points(self) -> np.ndarray:
-        inner_points = np.zeros(self.N ** 2)
-        for i in range(N):
-            for j in range(N):
-                # Translate the inner point into array form, but keep the x- and y- values
-                inner_points[self.g.n(i, j)] = ((i+1)*self.dx, (j+1)*self.dx)
-        return inner_points
+        self.grid = Grid(self.N)
+        self.A = np.eye(N**2) - self.dt * self.grid.laplacian()
+        self.inner_points = self.grid.generate_inner_points()
 
     def solve(self, u_old : np.ndarray, t : float) -> np.ndarray:
         # REPLACE WITH OWN IMPLEMENTATION!
@@ -125,59 +122,63 @@ class TimeStepper:
         
     def b(self, u_old : np.ndarray, t : float) -> np.ndarray:
         """Generates a right hand side for the problem, given a time t, which takes in to consideration the source term and the boundary terms"""
-        return u_old + self.f(self.inner_points, t) + self.g(self.boundary_points, t)
+        return u_old + self.f(self.inner_points, t) + self.g(self.grid.boundary_points, t)
 
     def step(self) -> None:
         times = np.linspace(0, self.T, num=self.M)
         u = self.u0
+        all_u = [self.u0]
         for t in times:
             # Visualize here if that is wanted, probably something like this:
             # if visualize:
             #     v.visualize(u)
             # or save all u:s and visualize in the end
             u = self.solve(u, t)
+            all_u.append(u)
+        return all_u
 
 
 if __name__ == '__main__':
-    N = 4
-    M = 10
-    T = 5
+    N = 30
+    M = 300
+    T = 3
     u0 = np.zeros(N**2)
 
     def f(xs : np.ndarray, t : float) -> np.ndarray:
         def f_internal(x_ : float, y_: float, t_ : float) -> bool:
-            return (x_-0.5)**2 + (y_-0.5)**2 < 0.1
-        out = np.zeros(xs.size)
+            return (x_-0.5 + 0.3*np.cos(2*np.pi*t_))**2 + (y_-0.5+np.sin(2*np.pi*t_))**2 < 0.03
+
+
+        out = np.zeros(len(xs))
         for n, (x, y) in enumerate(xs):
            if f_internal(x, y, t):
                out[n] = 1
+        return out
 
-    def g(boundary : np.ndarray, t : float) -> np.ndarray:
-        # TODO: Write a reasonable way to make the BC:s work!
-        return np.zeros(N**2)
+    def g(boundary : dict[int, list], t : float) -> np.ndarray:
+        def g_internal(x_, y_, t_):
+            return 0
 
-    g = Grid(N)
-  
-    # np.set_printoptions(threshold=np.inf)
+        out = np.zeros(N**2)
+        for n, boundary_points in boundary.items():
+            out[n] = sum([g_internal(x, y, t) for (x, y) in boundary_points])
+        return out
+
+    ts = TimeStepper(N, M, T, u0, f, g)
+    all_u = ts.step()
+
+    u_grids = []
+    for u in all_u:
+        u_grid = np.zeros((N, N))
+        for n, u_n in enumerate(u):
+            u_grid[n//N,n%N] = u_n
+        u_grids.append(u_grid)
+
+    fig, ax = plt.subplots()
+    artists = []
+    for u_grid in u_grids:
+        container = ax.imshow(u_grid, cmap='hot', interpolation='nearest')
+        artists.append([container])
     
-
-    # print(g.laplacian_fast() - g.laplacian())
-    # print(np.array_equal(g.laplacian_fast() - g.laplacian(), np.zeros((N**2, N**2), dtype=int)))
-    # g.test_speeds()
-    # eig_vals, eig_vecs = LA.eig(g.laplacian())
-    # sorted_eig_vals = sorted([float(-v) for v in eig_vals])
-    # print(sorted_eig_vals[0], sorted_eig_vals[-1], sorted_eig_vals[-1]/sorted_eig_vals[0])
-
-
-    #ts = TimeStepper(N, M, T, u0, f, g)
-
-    print(g.laplacian())
-    print()
-    sparse_laplacian = g.sparse_laplacian()
-    dense_laplacian = sparse_laplacian.toarray()
-    print(dense_laplacian)
-    print()
-    v = np.zeros(N**2)
-    v[1] = 1.0
-    w = sparse_laplacian @ v
-    print(w)
+    ani = animation.ArtistAnimation(fig=fig, artists=artists, interval=200, repeat=True)
+    plt.show()
